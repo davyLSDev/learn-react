2ac898571b64ea454908eec3e383916937701bc7
Davy Tenn <davy.lsdev@gmail.com>
2022-08-17 17:49:57 -0600

    Use conditional components
    At this point, the 'Greeting' function is not needed, but left in.

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
+var Clock = React.createClass({
+  render: function() {
+    var time = this.props.hour < 12 ? 'Morning' : 'Afternoon';
+    return (
+      React.createElement('h1', null, time)
+    );
+  }
+});
+
+var hour = (new Date).getHours();
+
 var Greeting = React.createClass({
   render: function() {
     return (
-      React.createElement('h1', null, this.props.name)
+      React.createElement('h1', null, 'Hello, world!')
     );
   }
 });
 
 window.addEventListener('load', function() {
   React.render(
-    React.createElement(Greeting, { name: 'world' }),
+    React.createElement(Clock, { hour: hour }),
     document.body
   );
 });
