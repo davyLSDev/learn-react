32e0b7d7fe2435831174831f49a215449260d2c0
Davy Tenn <davy.lsdev@gmail.com>
2022-08-17 17:40:51 -0600

    Set up to pass properties
    * HTML elements have attributes
    * Functions have arguements
    * React components have properties

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var Greeting = React.createClass({
   render: function() {
     return (
-      React.createElement('h1', null, 'Hello, world!')
+      React.createElement('h1', null, this.props.name)
     );
   }
 });
 
 window.addEventListener('load', function() {
   React.render(
-    React.createElement(Greeting, null),
+    React.createElement(Greeting, { name: 'world' }),
     document.body
   );
 });
